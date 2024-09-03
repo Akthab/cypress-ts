e3edb2fdac3dd6490393cdb4a496b65c8a23142d
Akthab <mhmdakthab6@gmail.com>
2024-09-03 12:26:19 +0530

    first commit

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,15 @@
+import * as lists from '../fixtures/lists.json';
+
+it('playing with typescript', () => {
+	cy.intercept('POST', '/api/lists').as('listCreate');
+
+	cy.visit('/board/1');
+
+	lists.forEach((list) => {
+		cy.get('[data-cy="add-list-input"]').type(`${list.name}{enter}`);
+
+		cy.wait('@listCreate')
+			.its('response.body.order')
+			.should('eq', list.order);
+	});
+});
